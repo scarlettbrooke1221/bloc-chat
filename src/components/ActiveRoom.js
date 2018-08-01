fe047ce8c8881fa4e3df926e50f0b15a52f52199
erica mitchell <ericamitchell1221@gmail.com>
2018-07-31 21:23:58 -0400

    add guest sign in

--- src/components/ActiveRoom.js
+++ src/components/ActiveRoom.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class ActiveRoom extends Component {
   constructor(props) {
     super(props);
--- src/components/User.js
+++ src/components/User.js
@@ -1,37 +1,38 @@
 import React, { Component } from 'react';
 
 class User extends Component {
-    constructor(props){
-        super(props);
-    };
 
-    componentDidMount() {
-        this.props.firebase.auth().onAuthStateChanged( user => {
-          this.props.setUser(user)
-        });
-      }
-
-    signIn(){
-        const provider = new this.props.firebase.auth.GoogleAuthProvider();
-this.props.firebase.auth().signInWithPopup( provider );
-        this.props.setUser("Guest");
-      alert('sign in please');
-    }
-
-    signOut(){
-        this.props.firebase.auth().signOut();
-        alert('Sign Out Please');
-    }
-    render(){
-        const displayedName = this.props.ActiveUser === (undefined) ? "Guest" :this.props.activeUser.displayName;
-      return (
-        <div>
-        <p> hello, {displayedName}</p>
-        <button type="text" onClick={this.signIn()}> Sign In </button>
-        <button type ="text" onClick= {this.signOut()}> Sign Out </button>
-        </div>
-      );
-    }
+  componentDidMount() {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user)
+    });
   }
 
-  export default User;
+  signIn(e) {
+  const provider = new this.props.firebase.auth.GoogleAuthProvider();
+  this.props.firebase.auth().signInWithPopup( provider);
+  console.log("signed in");
+  };
+
+  signOut(e) {
+    this.props.firebase.auth().signOut();
+    this.props.setUser("Guest");
+    console.log("signed out")
+  }
+
+  render() {
+
+    const displayName = this.props.activeUser === (undefined || null) ? "Guest" : this.props.activeUser.displayName;
+
+    return (
+        <div>
+        <p className = "userName">Hello, {displayName}</p>
+        <button type="text" onClick={() => this.signIn()}>Sign In</button>
+        <button type="text" onClick={() => this.signOut()}>Sign Out</button>
+        
+        </div>
+      
+    );
+  }
+};
+export default User;
