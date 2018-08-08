9502a0cd69fef6a71a2cff7d12ccca028a1f8c1a
erica mitchell <ericamitchell1221@gmail.com>
2018-08-07 23:55:13 -0400

    treid to fix username

--- src/components/User.js
+++ src/components/User.js
@@ -19,15 +19,20 @@ class User extends Component {
   }
 
   signIn() {
-    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+   const displayName = this.props.setUser === ( this.newMethod()) ? "Guest" : this.props.setUser.displayName;
+   const provider = new this.props.firebase.auth.GoogleAuthProvider();
     this.props.firebase.auth().signInWithPopup(provider).then((result) => {
-      const user = result.user;
-      this.props.setUser(user);
+      const user = result.displayName;
+      this.props.setUser(user, displayName);
       console.log("signed in");
     });
   }
 
 
+
+  newMethod() {
+    return null;
+  }
 
 signOut() {
   this.props.firebase.auth().signOut().then(() => {
@@ -37,13 +42,15 @@ signOut() {
 }
   
 
-
+  
 
 render() {
+
+
   return (
    <div>
-   <p>Hi, {this.setUser}. </p>
-   <p>{this.props.setUser === 'Guest' ? "Please sign in" : "You're signed in."}</p>
+   <p>Hi, {this.props.setUser.displayName}. </p>
+   <p>{this.props.setUser.displayName === 'Guest' ? "Please sign in" : "You're signed in."}</p>
      <button onClick={this.signIn}>Sign In</button>
      
      <button onClick={this.signOut}>Sign Out</button>
